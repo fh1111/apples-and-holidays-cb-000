0f5a6774478e19bcf0f4f0f73c425415fbd29a9e
Fer Hernandez <fh.1111@gmail.com>
2018-11-03 20:18:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -105,7 +105,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.each do |holiday, supplies|
       supplies.each do |supply|
         if (supply == "BBQ")
-          keys << holiday
+          keys << holiday_hash[season].keys
         end
       end
     end
