4016d3dcc58fd7ce03ea149ab9417c2d3b8a1990
Fer Hernandez <fh.1111@gmail.com>
2018-11-03 19:15:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 
 end
 
-add_supply_to_winter_holidays()
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
